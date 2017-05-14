e6111542a509cee7a244c3f5060532849b62d8f7
dan-42 <daniel@duerrenbuehl.de>
2017-05-14 17:04:36 +0200

    FEATURE: Update hunter package and minor changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,25 @@
 
+#include <thread>
 
+#include <user/client.hpp>
+#include <user/server.hpp>
 
+int main(int argc, char **argv) {
 
-int main() {
-  return 0;
+  try {
+
+    auto server = std::thread(&user::server::main());
+    auto client = std::thread(&user::client::main());
+
+    server.join();
+    client.join();
+
+    return 0;
+  }
+  catch(...) {
+    return 1;
+  }
+
 }
+
+
